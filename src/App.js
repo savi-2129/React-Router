713a249d9aa89777a1499f317d0e93fff231d271
savi-2129 <savi.21cs089@sode-edu.in>
2023-11-03 20:26:37 +0530

    attempts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import{BrowserRouter, Routes,Route}from'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pagegi/About/About';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <NavBar/>
+    <Routes>
+      <Route path="/Home" element={<Home/>}/>
+      <Route path="/About" element={<About/>}/>
+      
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
